020ead86b6e2b16622818707ac6ac22d63b62b83
xlisachan <xlisachan@gmail.com>
2019-04-24 16:59:38 -0400

    Revised js - font color adjusts based on background color

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 let clock = () => {
-    let d = new Date();
-    let h = d.getHours(); if (h < 10){h = "0"+ h;}
-    let m = d.getMinutes(); if (m < 10){m = "0" + m;}
-    let s = d.getSeconds(); if (s < 10){s = "0" + s;}
+    let d = new Date()
+        h = d.getHours()
+        m = d.getMinutes()
+        s = d.getSeconds();
+
+    if (h < 10) h = "0"+ h;
+    if (m < 10) m = "0" + m;
+    if (s < 10) s = "0" + s;
+
     let clock = document.getElementById('clock');
-    clock.innerHTML = `#${h}${m}${s}`;
+    clock.innerHTML = `#${h}:${m}:${s}`;
     document.body.style.backgroundColor = `#${h}${m}${s}`;
-    setTimeout('clock()',500);
+    document.body.style.color = ((h + m + s) < 127 * 3) ? 'white' : 'black';
+    
+    setTimeout('clock()', 500);
 }
 
 window.addEventListener('load', () => {
