856f4be140360f62d4aa4b218edec279a84d80da
xlisachan <xlisachan@gmail.com>
2019-04-24 17:50:55 -0400

    Added comments, revised font color change

--- main.js
+++ main.js
@@ -8,13 +8,25 @@ let clock = () => {
     if (m < 10) m = "0" + m;
     if (s < 10) s = "0" + s;
 
+    // set time
     let clock = document.getElementById('clock');
     clock.innerHTML = `#${h}:${m}:${s}`;
+
+    // set background color
     document.body.style.backgroundColor = `#${h}${m}${s}`;
-    document.body.style.color = ((h + m + s) > (127 * 3)) ? 'white' : 'black';
     
+    // determine and set font color by converting hex to rgb
+    let hex = `${h}${m}${s}`
+        rgb = parseInt(hex, 16)
+        r = (rgb >> 16) & 255
+        g = (rgb >> 8) & 255
+        b = rgb & 255;
+
+    document.body.style.color = ((r + g + b) < (127 * 3)) ? 'white' : 'black';
+
     setTimeout('clock()', 500);
 }
+
 
 window.addEventListener('load', () => {
     clock();
